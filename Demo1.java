2b3ecfa4f23fc40d9c2797f4a0d961a936cf648c
skgupta2580 <sg4905226@gmail.com>
2024-02-27 19:43:06 +0530

    abhi sir

---
+++ Demo1.java
@@ -0,0 +1,41 @@
+/*class Demo1
+{
+	public static void main(String ar[])
+	{
+		int i,j;
+		int a=0,b=1,c=0;
+		int n=25;
+		for(i=1;i<=n;i++)
+		{
+			c=0;
+		for(j=2;j<i;j++)
+		{
+			if(i%j==0)
+			{
+				c=1;
+			}
+		}
+		if(c==1)
+		{
+			System.out.printf("%d ",i);
+			a++;
+		}
+		if(a==b)
+		{
+			System.out.println();
+			a=0;
+			b++;
+		}
+	}}
+}
+*/
+
+class Demo1
+{
+	public static void main(String ar[])
+	{
+		char x=50;
+		System.out.println(x);
+	}
+}
+		
